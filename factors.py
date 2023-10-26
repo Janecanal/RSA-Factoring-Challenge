06fe84fb5039c49d74878d66c4256a8bd91753b7
Janecanal <janecanal04@gmail.com>
2023-10-26 09:14:03 -0700

    factoring challenge

--- factors.py
+++ factors.py
@@ -2,20 +2,31 @@
 
 import sys
 
-def factorize(n):
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return i, n//i
-    return None
+def factors(n):
+  """Factors the given number n into a product of two smaller numbers.
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: factors <file>')
-        sys.exit(1)
+  Args:
+    n: The number to factor.
 
-    with open(sys.argv[1], 'r') as f:
-        for line in f:
-            n = int(line.strip())
-            factors = factorize(n)
-            if factors is not None:
-                print(f'{n}={factors[0]}*{factors[1]}')
+  Returns:
+    A tuple of two numbers (p, q) such that n = p * q, or None if n is prime.
+  """
+
+  for i in range(2, int(n**0.5) + 1):
+    if n % i == 0:
+      return (i, n // i)
+  return None
+
+def main():
+  """Reads the numbers from the given file and factors them."""
+
+  file_name = sys.argv[1]
+  with open(file_name, "r") as f:
+    for line in f:
+      n = int(line)
+      factorization = factors(n)
+      if factorization is not None:
+        print(f"{n}={factorization[0]}*{factorization[1]}")
+
+if __name__ == "__main__":
+  main()
